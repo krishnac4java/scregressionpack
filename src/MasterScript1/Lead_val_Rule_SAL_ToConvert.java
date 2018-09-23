4273472e20969c0767a5d0f2327303c911936c9d
Krishna <krishna.c4java@gmail.com>
2018-09-23 22:12:21 +0100

    Usecase

--- src/MasterScript1/Lead_val_Rule_SAL_ToConvert.java
+++ src/MasterScript1/Lead_val_Rule_SAL_ToConvert.java
@@ -1,5 +1,8 @@
 package MasterScript1;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,17 +10,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Lead_val_Rule_SAL_ToConvert {
-		
+	
+	private static Date todayDate = new Date();
+	private static SimpleDateFormat sdf = new SimpleDateFormat("SSSssmmHHyyyMMdd");
+
 	public static void main (String[] args) throws Exception {
 		
 				
 		//ObjectRepositoryMain ORM = new ObjectRepositoryMain ();
 		
-		ObjectRepositoryMain.launchChrome();
+		/*ObjectRepositoryMain.launchChrome();
 		ObjectRepositoryMain.loginQA();
 		ObjectRepositoryMain.clickLeadsTab();
 		ObjectRepositoryMain.selectLeadRTandClickNext();
-		ObjectRepositoryMain.createLead();		
+		ObjectRepositoryMain.createLead();	*/	
 	}
 	
 	@Test
@@ -30,23 +36,54 @@ public class Lead_val_Rule_SAL_ToConvert {
 	}
 	
 	@Test
-	public void FinalUATAccountCreation() throws Exception {
+	public void AccountCreation_CustomerRT() throws Exception {
 		ObjectRepositoryMain.launchChrome();
-		ObjectRepositoryMain.loginFinalUAT();
+		ObjectRepositoryMain.loginQA();
 		ObjectRepositoryMain.clickAccountsTab();
 		ObjectRepositoryMain.selectCustomerRecord();
-		ObjectRepositoryMain.fillAccountsFormAndSave();
+		todayDate = new Date();
+		ObjectRepositoryMain.fillAccountsFormAndSave("CRT", sdf.format(todayDate));
 		ObjectRepositoryMain.createNewContact();
 		ObjectRepositoryMain.selectContactRecordType();
-		ObjectRepositoryMain.fillContactFormAndSave();
+		todayDate = new Date();
+		ObjectRepositoryMain.fillContactFormAndSave(sdf.format(todayDate));
+		ObjectRepositoryMain.fillNewOpportunityFormAndSave(sdf.format(todayDate));
+		ObjectRepositoryMain.fillNewProductAndSave(sdf.format(todayDate));
 	}
 	
 	@Test
+	public void AccountCreation_AccRqstRcrd() throws Exception {
+		ObjectRepositoryMain.launchChrome();
+		ObjectRepositoryMain.loginQA();
+		ObjectRepositoryMain.clickAccountsTab();
+		ObjectRepositoryMain.selectAccRqstRT();
+		ObjectRepositoryMain.fillAccountsFormAndSave("AccRT", sdf.format(todayDate));
+	}
+	
+	@Test
+	public void AccountCreation_Competitor() throws Exception {
+		ObjectRepositoryMain.launchChrome();
+		ObjectRepositoryMain.loginQA();
+		ObjectRepositoryMain.clickAccountsTab();
+		ObjectRepositoryMain.selectCompetitorRT();
+		ObjectRepositoryMain.fillAccountsFormAndSave("CompRT", sdf.format(todayDate));
+	}
+	
+	@Test
+	public void AccountCreation_Program() throws Exception {
+		ObjectRepositoryMain.launchChrome();
+		ObjectRepositoryMain.loginQA();
+		ObjectRepositoryMain.clickAccountsTab();
+		ObjectRepositoryMain.selectProgramRT();
+		ObjectRepositoryMain.fillAccountsFormAndSave("PRT", sdf.format(todayDate));
+	}
+	
+	/*@Test
 	public void TESTUATAccountCreation() throws Exception {
 		ObjectRepositoryMain.launchChrome();
-		ObjectRepositoryMain.loginFinalUAT();
+		ObjectRepositoryMain.loginQA();
 		ObjectRepositoryMain.scrollTest();
-	}
+	}*/
 	
 }
 	
--- src/MasterScript1/ObjectRepositoryMain.java
+++ src/MasterScript1/ObjectRepositoryMain.java
@@ -23,8 +23,6 @@ public class ObjectRepositoryMain {
 	String leadLandline_xPath = "/html[1]/body[1]/div[5]/div[2]/div[4]/div[2]/div[1]/div[2]/div[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[5]/div[1]/div[1]/div[1]/div[1]/input[1]";
 	static String lastnameError_expected = "Complete this field";
 	static String LastNameError;
-	private static SimpleDateFormat sdf = new SimpleDateFormat("SSSssmmHHyyyMMdd");
-	private static Date todayDate = new Date();
 	
 	@Before
 	  public static void launchChrome() throws Exception {
@@ -42,7 +40,7 @@ public class ObjectRepositoryMain {
 		driver.findElement(By.id("username")).clear();
 	    driver.findElement(By.id("username")).sendKeys("vima@sitecore.net.sitecoreqa");
 	    driver.findElement(By.id("password")).clear();
-	    driver.findElement(By.id("password")).sendKeys("Ovj4519oe26qa");	   
+	    driver.findElement(By.id("password")).sendKeys("Ovj4519oe26qa3");	   
 	    driver.findElement(By.id("Login")).click();
 	    Thread.sleep(2000);
 	}	
@@ -80,15 +78,15 @@ public class ObjectRepositoryMain {
 		System.out.println(LastNameError);
 		
 	}
-	public static void loginFinalUAT() throws InterruptedException {
+	/*public static void loginFinalUAT() throws InterruptedException {
 		
 		driver.findElement(By.id("username")).clear();
-	    driver.findElement(By.id("username")).sendKeys("vima@sitecore.net.finaluat");
+	    driver.findElement(By.id("username")).sendKeys("vima@sitecore.net.sitecoreqa");
 	    driver.findElement(By.id("password")).clear();
-	    driver.findElement(By.id("password")).sendKeys("Ovj4519oe26");	    
+	    driver.findElement(By.id("password")).sendKeys("Ovj4519oe26qa4");	    
 	    driver.findElement(By.id("Login")).click();
 	    Thread.sleep(2000);
-	}
+	}*/
 	
 	public static void clickAccountsTab() throws InterruptedException {
 		Thread.sleep(5000);
@@ -105,18 +103,38 @@ public class ObjectRepositoryMain {
 		driver.findElement(By.xpath("//div[@class='forceChangeRecordTypeFooter']//button[2]")).click();
 		Thread.sleep(3000);
 	}
+	
+	public static void selectAccRqstRT() throws InterruptedException {
+		driver.findElement(By.xpath("//span[contains(text(),'Account Request Record')]")).click();
+		driver.findElement(By.xpath("//div[@class='forceChangeRecordTypeFooter']//button[2]")).click();
+		Thread.sleep(3000);
+	}
+	
+	public static void selectCompetitorRT() throws InterruptedException {
+		driver.findElement(By.xpath("//span[contains(text(),'Competitor Record')]")).click();
+		driver.findElement(By.xpath("//div[@class='forceChangeRecordTypeFooter']//button[2]")).click();
+		Thread.sleep(3000);
+	}
+	
+	public static void selectProgramRT() throws InterruptedException {
+		driver.findElement(By.xpath("//span[contains(text(),'Program Record')]")).click();
+		driver.findElement(By.xpath("//div[@class='forceChangeRecordTypeFooter']//button[2]")).click();
+		Thread.sleep(3000);
+	}
 
-	public static void fillAccountsFormAndSave() {
-		driver.findElement(By.xpath("//div[@class='autocompleteWrapper slds-grow']//input[1]")).sendKeys(sdf.format(todayDate)+"Test");
-		driver.findElement(By.xpath("//div[4]/div[2]/div/div/div/div/div/div/div/a")).click();
-		driver.findElement(By.xpath("//a[contains(text(),'Verified')]")).click();
+	public static void fillAccountsFormAndSave(String postfix, String timestamp) {
+		driver.findElement(By.xpath("//div[@class='autocompleteWrapper slds-grow']//input[1]")).sendKeys(timestamp+postfix);
+		if(!postfix.equals("PRT")) {
+			driver.findElement(By.xpath("//div[4]/div[1]/div[1]/div[1]/div[1]/div[5]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]")).click();
+			driver.findElement(By.xpath("//a[contains(text(),'Verified')]")).click();
+		}
 		driver.findElement(By.xpath("//fieldset/div/div[2]/div/div/div/div/div/div/a")).click();
 		driver.findElement(By.xpath("//a[contains(text(),'United Kingdom')]")).click();
-		driver.findElement(By.xpath("//div/div/div/div/fieldset/div/div[3]/div/div/textarea")).sendKeys(sdf.format(todayDate));
-		driver.findElement(By.xpath("//div/div/div/div/fieldset/div/div[4]/div/div/input")).sendKeys(sdf.format(todayDate));
+		driver.findElement(By.xpath("//div/div/div/div/fieldset/div/div[3]/div/div/textarea")).sendKeys(timestamp);
+		driver.findElement(By.xpath("//div/div/div/div/fieldset/div/div[4]/div/div/input")).sendKeys(timestamp);
 		driver.findElement(By.xpath("//div/div/div/div/fieldset/div/div[4]/div[2]/div/div/div/div/div/a")).click();
 		driver.findElement(By.xpath("//a[contains(text(),'England')]")).click();
-		driver.findElement(By.xpath("//div/div/div/div/fieldset/div/div[5]/div/div/input")).sendKeys(sdf.format(todayDate));
+		driver.findElement(By.xpath("//div/div/div/div/fieldset/div/div[5]/div/div/input")).sendKeys(timestamp);
 		driver.findElement(By.xpath("//div[@class='slds-form-element__control']/div/input")).sendKeys("1234567890");
 		driver.findElement(By.xpath("//button[@title='Save']")).click();
 	}
@@ -133,45 +151,22 @@ public class ObjectRepositoryMain {
 		
 	}
 
-	public static void fillContactFormAndSave() throws InterruptedException {
-		driver.findElement(By.xpath("//div/div/div/div/div/fieldset/div/div[4]/input")).sendKeys(sdf.format(todayDate));
-		driver.findElement(By.xpath("//div/div/div/div/div[2]/div/div/div/div/input")).sendKeys(sdf.format(todayDate));
-		driver.findElement(By.xpath("//div[5]/div[2]/div/div/div/input")).sendKeys(sdf.format(todayDate));
+	public static void fillContactFormAndSave(String timestamp) throws InterruptedException {
+		driver.findElement(By.xpath("//div/div/div/div/div/fieldset/div/div[4]/input")).sendKeys(timestamp);
+		driver.findElement(By.xpath("//div/div/div/div/div[2]/div/div/div/div/input")).sendKeys(timestamp);
+		driver.findElement(By.xpath("//div[5]/div[2]/div/div/div/input")).sendKeys(timestamp);
 		driver.findElement(By.xpath("//div[4]/div/div/div[5]/div/div/div/div/input")).click();
 		driver.findElement(By.xpath("//div[4]/div[2]/div/div/div/div/div/div/div/a")).click();
 		driver.findElement(By.xpath("//div/ul/li[5]/a")).click();
 		driver.findElement(By.xpath("//button[@title='Save']")).click();
-		
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("//span[@class='slds-truncate'][contains(text(),'Contacts')]")).click();
-		driver.findElement(By.xpath("//div[2]/force-list-view-manager-search-bar/div/lightning-input/div/input")).sendKeys(sdf.format(todayDate));
-		driver.findElement(By.xpath("//a[@title='"+sdf.format(todayDate)+"']")).click();
-		driver.findElement(By.xpath("//li[@class='tabs__item uiTabItem']//span[@class='title'][contains(text(),'Related')]")).click();
-		driver.findElement(By.xpath("//div[@class='windowViewMode-normal oneContent active lafPageHost']//div[@class='container']//div[1]//article[1]//div[1]//div[1]//div[1]//ul[1]//li[1]//a[1]")).click();
-		driver.findElement(By.xpath("//div[@class='forceChangeRecordTypeFooter']//button[2]")).click();
-		driver.findElement(By.xpath("//div/div/div/div/div/div/div/div/div/input")).sendKeys(sdf.format(todayDate));
-		driver.findElement(By.xpath("//div[2]/div/div/div/div/div/div/div/div/a")).click();
-		driver.findElement(By.xpath("//a[contains(text(),'License')]")).click();
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//div[3]/div/div/div/div/div/div/div/div/a")).click();
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//li[@class='uiMenuItem uiRadioMenuItem']//a[@title='New']")).click();
-		driver.findElement(By.xpath("//div[4]/div/div/div/div/div/div/div/div/a")).click();
-		driver.findElement(By.xpath("//a[contains(text(),'Perpetual')]")).click();
-		driver.findElement(By.xpath("//div[4]/div[2]/div/div/div/div/div/div/div/a")).click();
-		driver.findElement(By.xpath("//a[contains(text(),'Qualification')]")).click();
-		driver.findElement(By.xpath("//div[7]/div[2]/div/div/div/div/div/div/div/a")).click();
-		driver.findElement(By.xpath("//a[contains(text(),'Sales')]")).click();
-		driver.findElement(By.xpath("//div[4]/div/div/div/div/div/div/div/div/div/div/div/a")).click();
-		driver.findElement(By.xpath("//a[contains(text(),'No Competitor')]")).click();
-		driver.findElement(By.xpath("//div[2]/div/div/div/div[2]/div/div/div/div/input")).click();
-		driver.findElement(By.xpath("//div[2]/table/tbody/tr[6]/td/span")).click();
-		driver.findElement(By.xpath("//button[@title='Save']")).click();
-		
+
+	}
+
+	public static void fillNewProductAndSave(String timestamp) throws InterruptedException {
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//span[@class='slds-truncate'][contains(text(),'Opportunities')]")).click();
-		driver.findElement(By.xpath("//lightning-input/div/input")).sendKeys(sdf.format(todayDate));
-		driver.findElement(By.xpath("//a[@title='"+sdf.format(todayDate)+"']")).click();
+		driver.findElement(By.xpath("//lightning-input/div/input")).sendKeys(timestamp);
+		driver.findElement(By.xpath("//a[@title='"+timestamp+"']")).click();
 		Thread.sleep(8000);
 		((JavascriptExecutor)driver).executeScript("scroll(0,100)");
 		Thread.sleep(8000);
@@ -191,9 +186,36 @@ public class ObjectRepositoryMain {
 		driver.findElement(By.xpath("//tr/td[3]/div/div/div/div/div/div/input")).clear();
 		driver.findElement(By.xpath("//tr/td[3]/div/div/div/div/div/div/input")).sendKeys("500");
 		driver.findElement(By.xpath("//button[contains(@title,'Save')]")).click();
-		//driver.findElement(By.xpath("//div/button[3]")).click();
+	}
 
-
+	public static void fillNewOpportunityFormAndSave(String timestamp) throws InterruptedException {
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//span[@class='slds-truncate'][contains(text(),'Contacts')]")).click();
+		driver.findElement(By.xpath("//div[2]/force-list-view-manager-search-bar/div/lightning-input/div/input")).sendKeys(timestamp);
+		driver.findElement(By.xpath("//a[@title='"+timestamp+"']")).click();
+		driver.findElement(By.xpath("//input[@id='input-4']")).sendKeys(timestamp);
+		driver.findElement(By.xpath("//input[@id='input-48']")).sendKeys("Sep 30, 2018");
+		/*driver.findElement(By.xpath("//li[@class='tabs__item uiTabItem']//span[@class='title'][contains(text(),'Related')]")).click();
+		driver.findElement(By.xpath("//div[@class='windowViewMode-normal oneContent active lafPageHost']//div[@class='container']//div[1]//article[1]//div[1]//div[1]//div[1]//ul[1]//li[1]//a[1]")).click();
+		driver.findElement(By.xpath("//div[@class='forceChangeRecordTypeFooter']//button[2]")).click();
+		driver.findElement(By.xpath("//div/div/div/div/div/div/div/div/div/input")).sendKeys(timestamp);
+		driver.findElement(By.xpath("//div[2]/div/div/div/div/div/div/div/div/a")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'License')]")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//div[3]/div/div/div/div/div/div/div/div/a")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//li[@class='uiMenuItem uiRadioMenuItem']//a[@title='New']")).click();
+		driver.findElement(By.xpath("//div[4]/div/div/div/div/div/div/div/div/a")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'Perpetual')]")).click();
+		driver.findElement(By.xpath("//div[4]/div[2]/div/div/div/div/div/div/div/a")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'Qualification')]")).click();
+		driver.findElement(By.xpath("//div[7]/div[2]/div/div/div/div/div/div/div/a")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'Sales')]")).click();
+		driver.findElement(By.xpath("//div[4]/div/div/div/div/div/div/div/div/div/div/div/a")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'No Competitor')]")).click();
+		driver.findElement(By.xpath("//div[2]/div/div/div/div[2]/div/div/div/div/input")).click();
+		driver.findElement(By.xpath("//div[2]/table/tbody/tr[6]/td/span")).click();
+		driver.findElement(By.xpath("//button[@title='Save']")).click();*/
 	}
 	public static void scrollTest() throws InterruptedException {
 		((JavascriptExecutor)driver).executeScript("scroll(0,100)");
