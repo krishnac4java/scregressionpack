41a6a377f0cb84ba76dc4e415ff8f3905def50d4
Krishna <krishna.c4java@gmail.com>
2018-10-13 23:23:29 +0100

    Model

---
+++ src/com/sitcore/selinium/java/TestResultModel.java
@@ -0,0 +1,75 @@
+package com.sitcore.selinium.java;
+
+public class TestResultModel {
+	private String loginName;
+	private String methodName;
+	private String accountName;
+	private String accountType;
+	private String contactName;
+	private String contactType;
+	private String opportunityName;
+	private String product1;
+	private String product2;
+	private String testResult;
+	public String getMethodName() {
+		return methodName;
+	}
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+	public String getAccountName() {
+		return accountName;
+	}
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+	public String getContactName() {
+		return contactName;
+	}
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+	public String getOpportunityName() {
+		return opportunityName;
+	}
+	public void setOpportunityName(String opportunityName) {
+		this.opportunityName = opportunityName;
+	}
+	public String getProduct1() {
+		return product1;
+	}
+	public void setProduct1(String product1) {
+		this.product1 = product1;
+	}
+	public String getProduct2() {
+		return product2;
+	}
+	public void setProduct2(String product2) {
+		this.product2 = product2;
+	}
+	public String getLoginName() {
+		return loginName;
+	}
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+	public String getTestResult() {
+		return testResult;
+	}
+	public void setTestResult(String testResult) {
+		this.testResult = testResult;
+	}
+	public String getAccountType() {
+		return accountType;
+	}
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	public String getContactType() {
+		return contactType;
+	}
+	public void setContactType(String contactType) {
+		this.contactType = contactType;
+	}
+	
+}
