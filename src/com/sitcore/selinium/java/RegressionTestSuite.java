0070f61f396668f8dd124820ada0de6f1bd3f757
Krishna <krishna.c4java@gmail.com>
2018-10-21 20:23:57 +0100

    Suite

--- src/com/sitcore/selinium/java/RegressionTestSuite.java
+++ src/com/sitcore/selinium/java/RegressionTestSuite.java
@@ -6,15 +6,21 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({
-   //Opportunity_Creation_All_Record_Types.class,
-   //US_1292.class,
+   Opportunity_Creation_All_Record_Types.class,
+   US_1292.class,
    US_1293.class,
-   SendEmail.class/*,
    US_1294.class,
    US_1383.class,
    US_1087.class,
    US_1090.class,
-   US_512.class*/
+   US_1258.class,
+   US_1246.class,
+   US_1263.class,
+   US_1295.class,
+   US_1343.class,
+   US_512.class,
+   US_779.class,
+   SendEmail.class
 })
 public class RegressionTestSuite {
 
