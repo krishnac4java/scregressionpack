d2aff1b22af64e3769cf3c5eee40f92585c5af21
Krishna <krishna.c4java@gmail.com>
2018-11-05 12:41:58 +0000

    NameGenerator

---
+++ src/com/sitcore/selinium/java/RandomCharcterGenerator.java
@@ -0,0 +1,30 @@
+package com.sitcore.selinium.java;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RandomCharcterGenerator {
+
+	final String lexicon = "abcdefghijklmnopqrstuvwxyz";
+
+	final java.util.Random rand = new java.util.Random();
+
+	// consider using a Map<String,Boolean> to say whether the identifier is being used or not 
+	final Set<String> identifiers = new HashSet<String>();
+
+	public String randomIdentifier() {
+	    StringBuilder builder = new StringBuilder();
+	    while(builder.toString().length() == 0) {
+	        int length = rand.nextInt(5)+5;
+	        for(int i = 0; i < length; i++) {
+	            builder.append(lexicon.charAt(rand.nextInt(lexicon.length())));
+	        }
+	        if(identifiers.contains(builder.toString())) {
+	            builder = new StringBuilder();
+	        }
+	    }
+	    return builder.toString();
+	}
+	
+	
+}
