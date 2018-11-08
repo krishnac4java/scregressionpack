4dff86924aec8763fc62f087dd37d550e00ccfb1
Krishna <krishna.c4java@gmail.com>
2018-11-08 23:37:02 +0000

    1135

---
+++ src/com/sitcore/selinium/java/US_1135.java
@@ -0,0 +1,52 @@
+package com.sitcore.selinium.java;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+public class US_1135 {
+	public TestResultModel resultModel = new TestResultModel();
+	
+	@Rule
+    public TestRule watcher = new TestWatcher() {
+        @Override
+        protected void succeeded(Description description) {
+        	if(!description.getMethodName().equals("sendEmail")) {
+        		resultModel.setTestResult("Success");
+        		ObjectRepositoryMain.userStoryresultList.add(resultModel);
+        		resultModel = new TestResultModel();
+        		//emailbody += description.getMethodName()+":			Pass!			OpportunityName: "+opportunityName+"\n";
+        		
+        	}
+        }
+
+        @Override
+        protected void failed(Throwable e, Description description) {
+        	if(!description.getMethodName().equals("sendEmail"))
+        		resultModel.setTestResult("Fail");
+        		ObjectRepositoryMain.userStoryresultList.add(resultModel);
+				resultModel = new TestResultModel();
+        		//emailbody += description.getMethodName()+":			Fail! \n";
+				try {
+					ObjectRepositoryMain.closeChrome();
+				} catch (Exception e1) {
+					
+				}
+        }
+    };
+	@Test
+	public void salesOps_VerfyAccountFields() throws Exception {
+    	resultModel.setLoginName("Sales Operations");
+		resultModel.setMethodName("User Story - 1135");
+		ObjectRepositoryMain.launchChrome();
+		ObjectRepositoryMain.loginUser_SDR();
+		ObjectRepositoryMain.clickLeadsTab();
+		ObjectRepositoryMain.leadGenerationLeadGenType();
+		ObjectRepositoryMain.fieldValidation_US_1135();
+		ObjectRepositoryMain.closeChrome();
+	}
+
+}
